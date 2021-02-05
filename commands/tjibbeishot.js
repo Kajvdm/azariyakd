20a55051257fcc2ad63d4f027921799fcd4736ab
Kajvdm <66601386+Kajvdm@users.noreply.github.com>
2021-02-05 13:27:45 +0100

    Update tjibbeishot.js

--- commands/tjibbeishot.js
+++ commands/tjibbeishot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 
 module.exports = {
-    name: 'discord',
-    description: "Sets up a reaction role message!",
+    name: 'tjibbeishot',
+    description: "Cap",
     async execute(message, args, Discord, client) {
         message.channel.send(":billed_cap:");
     }
